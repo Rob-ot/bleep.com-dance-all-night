9e1f44dd5609ae683e9bb147f7c84955b3b7bdaa
Rob Middleton <rob@middlerob.com>
2017-05-07 09:50:58 -0700

    Fix script to work with latest website updates

--- bleep.com-dance-all-night.user.js
+++ bleep.com-dance-all-night.user.js
@@ -2,15 +2,13 @@
 // @name        bleep.com dance all night
 // @namespace   rob@middlerob.com
 // @include     https://bleep.com/*
-// @version     1
+// @version     2
 // @description Disable the 60 second pause on bleep.com
 // @license     MIT
 // @oujs:author middlerob
 // @grant       none
 // ==/UserScript==
 
-eval(durationChanged.toString().replace('durationChanged', 'fixedDurationChanged').replace(60, 1000000))
-
-$('#jplayer')
-  .unbind($.jPlayer.event.timeupdate)
-  .bind($.jPlayer.event.timeupdate, fixedDurationChanged)
+setInterval(function() {
+  window.playableTime = 1000000
+}, 2000)
